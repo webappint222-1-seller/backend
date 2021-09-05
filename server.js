9482ef09a533c910d2323c57796c1a78b087ceaf
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-09-05 21:38:53 +0700

    remember to install formdata
    npm install --save form-data

--- server.js
+++ server.js
@@ -1,40 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const mysql =require('mysql');
 
-const app = express();
+var mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'root',
+    database:'mydb'
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
-
-// simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
 });
-require("./app/routes/turorial.routes")(app);
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+
+mysqlConnection.connect((err) => {
+    if (!err)
+        console.log('yyes');
+    else
+        console.log('no');
 });
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+
+app.listen(3000,()=> console.log('asd'));
+

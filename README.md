1fe3cdff139f53ad4f5433424c6c4e583775c658
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-11-03 14:45:07 +0700

    hot fixed

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ Start with
 node sever.js
 Every image will save in public
 ("/") is test connection
+-product
 ("/formdataupload") is to upload formdata
 get("/products")to list all product
 get("/products/:productId") list 1 product
 delete ("/products/:productId") to delete 1 product by ID
+put ("/update/:productId") update product by ID
+-user
+create('/formdatausersupload')
+get("/customers")
+get("/customers/:customerId")
+<!-- delete("/customers/:customerId") -->
+
 
--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const customers = require("../controllers/customer.controller.js");
 
 
-  app.post("/customers", customers.create);
+  // app.post("/customers", customers.create);
 
 
   app.get("/customers", customers.findAll);
@@ -11,7 +11,7 @@ module.exports = app => {
   app.get("/customers/:customerId", customers.findOne);
 
 
-  app.put("/customers/:customerId", customers.update);
+  // app.put("/customers/:customerId", customers.update);
 
   app.delete("/customers/:customerId", customers.delete);
 

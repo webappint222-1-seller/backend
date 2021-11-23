0972dab7778f4d9fef2237b8c84e1d75df4f0151
Thanadol <thanadol.29@mail.kmutt.ac.th>
2021-11-23 20:12:41 +0700

    test

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 // const fs = require('fs');
 // const https = require('https');
-
+const status = require('http-status');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cors());
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', "http://localhost:8080");
+  res.header('Access-Control-Allow-Origin', "https://www.utastore.team/");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -27,9 +27,18 @@ app.use(function (req, res, next) {
 //   key: fs.readFileSync('etc/key.pem'),
 //   cert: fs.readFileSync('etc/cert.pem')
 // };
+
+const whitelist ="https://www.utastore.team/";
 const corsOptions = {
-  origin: 'http://localhost:8080',
+  origin: (origin,callback,res)=> {
+    if(!origin || whitelist.indexOf(origin) !== -1){
+      callback(null, true)
+    }else{
+      callback(status[401] + " you dont have right to enter this page")
+    }
+  },
   credentials: true,
+  optionsSuccessStatus: 200
 };
 
 app.use(cookieParser())
@@ -54,11 +63,8 @@ const storage = multer.diskStorage({
   destination: function (request, file, callback) {
     callback(null, './public/upload')
   },
-
   filename: function (res, file, callback) {
-
     callback(null, suff + ".png")
-
   }
 });
 
@@ -68,21 +74,7 @@ const multerSigleUpload = multer({
 app.use('/upload', express.static('public'));
 
 
-// app.post("/singleFile", multerSigleUpload.single('singleImage'), function (req, res) {
-//   const file = req.file
-//   const body = req.body
-//   req.app.locals.uploadStatus = true;
-//   res.redirect('/uploaded');
-// });
-
 app.post('/formdataupload', multerSigleUpload.single('image'), function (req, res) {
-  // var db = "INSERT INTO `product`(`product_name`, `band_name`, `price`,`product_des`,`image`) VALUES ('" + req.body.product_name + "', '" + req.body.band_name + "', '" + req.body.price + "','" + req.body.product_des + "','" + suff + ".png" + "')";
-  // sql.query(db, function (err, result) {
-  //   console.log('inserted data');
-  //   console.log(db);
-  //   console.log(result);
-  // });
-
   if (!req.cookies['jwt']) {
     return res.status(401).send("must login")
   } else {
@@ -110,51 +102,87 @@ app.post('/formdataupload', multerSigleUpload.single('image'), function (req, re
 });
 
 app.put('/productupdate/:productId', multerSigleUpload.single('image'), function (req, res, next) {
-  console.log('file received');
-  console.log(req);
-  var db2 = "UPDATE product SET `product_name` = '" + req.body.product_name + "', `band_name` = '" + req.body.band_name + "' , `price` = '" + req.body.price + "',product_des = '" + req.body.product_des + "',image = '" + suff + ".png" + "' WHERE product_id = '" + req.params.productId + "'"
-  sql.query(db2, function (err, result2) {
-    console.log('inserted data');
-    console.log(db2);
-    console.log(result2);
-  });
+  if (!req.cookies['jwt']) {
+    return res.status(401).send("must login")
+  } else {
+    const theCookie = req.cookies['jwt'];
+    const decoded = jwt.verify(theCookie, 'secrect');
+    if (!decoded) {
+      return res.status(401).send("unauthebtucated")
+    }
+    sql.connect((err) => {
+      sql.query('SELECT * FROM user where user_id=' + decoded.id, function (error, results) {
+        if (results[0].role == 1) {
+          var db = "UPDATE product SET `product_name` = '" + req.body.product_name + "', `band_name` = '" + req.body.band_name + "' , `price` = '" + req.body.price + "',product_des = '" + req.body.product_des + "',image = '" + suff + ".png" + "' WHERE product_id = '" + req.params.productId + "'"
+          sql.query(db, function (err, result) {
+            console.log(db);
+            console.log(result);
+          });
+        } else {
+          return res.status(401).send("must be admin to edit product")
+        }
+      });
+    });
+  }
   res.redirect('/');
+
 });
 
-app.post('/formdatausersupload', loginValidation, multerSigleUpload.single('image'), function (req, res) {
+app.delete('/products/:productId', multerSigleUpload.single('image'), (req, res) => {
+  if (!req.cookies['jwt']) {
+    return res.status(401).send("must login")
+  } else {
+    const theCookie = req.cookies['jwt'];
+    const decoded = jwt.verify(theCookie, 'secrect');
+    if (!decoded) {
+      return res.status(401).send("unauthebtucated")
+    }
+    sql.connect((err) => {
+      sql.query('SELECT * FROM user where user_id=' + decoded.id, function (error, results) {
+        if (results[0].role == 1) {
+          var db = "delete from orderdetail_has_product where product_product_id='" + req.params.productId + "'"
+          sql.query(db, function (err, result) {
+            console.log(db);
+            console.log(result);
+          });
+          var db = "delete from product where product_id='" + req.params.productId + "'"
+          sql.query(db, function (err, result) {
+            console.log(db);
+            console.log(result);
+          });
+
+        } else {
+          return res.status(401).send("must be admin to delete product")
+        }
+      });
+    });
+  }
+});
+
+app.post('/formdatausersupload', signupValidation, multerSigleUpload.single('image'), function (req, res) {
   console.log('file received');
   console.log(req);
-  var asd = "SELECT emailaddress FROM user where emailaddress = '" + req.body.emailaddress + "'"
+  var asd = "SELECT emailaddress FROM user where emailaddress = lower('" + req.body.emailaddress + "')"
   sql.connect((err) => {
     sql.query(asd, function (err, result) {
       if (result == 0) {
         bcrypt.hash(req.body.password, 10, function (err, hash) {
-          var db1 = "INSERT INTO user (`emailaddress`, `password`, `name`,`phonenumber`,`DOB`,`address`,`role`) VALUES ('" + req.body.emailaddress + "', '" + hash + "', '" + req.body.name + "','" + req.body.phonenumber + "','" + req.body.dob + "','" + req.body.address + "','" + 2 + "')";
+          var db1 = "INSERT INTO user (`emailaddress`, `password`, `name`,`phonenumber`,`DOB`,`address`,`role`) VALUES (LOWER('" + req.body.emailaddress + "'), '" + hash + "', '" + req.body.name + "','" + req.body.phonenumber + "','" + req.body.dob + "','" + req.body.address + "','" + 2 + "')";
           sql.query(db1, function (err, result1) {
             console.log("pass");
           });
         });
-      }else{
-        res.status(400).json("not pass");
+      } else {
+        res.status(400).json("something when wrong");
       }
     })
   });
-
-  // bcrypt.hash(req.body.password, 10, function (err, hash) {
-  //   var db1 = "INSERT INTO user (`emailaddress`, `password`, `name`,`phonenumber`,`DOB`,`address`,`role`) VALUES ('" + req.body.emailaddress + "', '" + hash + "', '" + req.body.name + "','" + req.body.phonenumber + "','" + req.body.dob + "','" + req.body.address + "','" + 2 + "')";
-  //   sql.query(db1, function (err, result1) {
-  //     console.log('inserted data');
-  //     console.log(db1);
-  //     console.log(result1);
-  //   });
-  // });
-  res.redirect('/');
 });
 
-app.post('/login', multerSigleUpload.single('image'), function (req, res) {
+app.post('/login', loginValidation, multerSigleUpload.single('image'), function (req, res) {
   console.log('file received');
   console.log(req);
-  var db = "SELECT * FROM user where emailaddress = '" + req.body.emailaddress + "'"
+  var db = "SELECT * FROM user where emailaddress = lower('" + req.body.emailaddress + "')"
   sql.connect((err) => {
     sql.query(db, function (err, result1) {
       bcrypt.compare(req.body.password, result1[0].password, function (err, result) {
@@ -170,43 +198,7 @@ app.post('/login', multerSigleUpload.single('image'), function (req, res) {
     });
   });
 
-  // bcrypt.compare(req.body.password, "$2a$10$VD53K9UoFs6UdhTdh7Y/9.2VAQmnqwmlJd506.mrAdIJT1BKSpknm", function (err, result) {
-  //   if(result == true){
-  //     var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
-  //     console.log(token);
-  //   }
-  // });
-
-
 });
-
-// app.put('/userupdate/:userId', multerSigleUpload.single('image'), function (req, res) {
-//   console.log('file received');
-//   console.log(req);
-//   var db4 = "UPDATE user SET `password` = '" + req.body.password + "' WHERE user_id = '" + req.params.userId + "'"
-
-//   sql.query(db4, function (err, result4) {
-//     console.log('inserted data');
-//     console.log(db4);
-//     console.log(result4);
-//   });
-//   res.redirect('/');
-// });
-
-
-// app.post('/register' , multerSigleUpload.single('image'), function(req, res)  {
-//   bcrypt.hash(req.body.password, 10,function(err,hash){
-//     var db6 = "INSERT INTO user (name, emailaddress, password, phonenumber, DOB, address, role) VALUES ("+ req.body.name +", "+req.body.emailaddress+","+ req.body.password +","+req.body.phonenumber+","+req.body.dob+","+req.body.address+",'buyer')"
-//     sql.query(db6,function (err, result) {
-//       console.log('inserted data');
-//       console.log(db6);
-//       console.log(result);
-
-//     });
-//   });
-//   var db5 = "SELECT * FROM users WHERE LOWER(emailaddress) = LOWER("+req.body.emailaddress+");"
-//   res.redirect('/');
-// });
 
 // app.get('/getalluser', multerSigleUpload.single('image'), function (req, res) {
 //   // if (!req.cookies['jwt']) {
@@ -287,7 +279,36 @@ app.post('/checkout', multerSigleUpload.single('image'), (req, res, next) => {
   res.redirect('/');
 });
 
-app.delete('/checkoutdelete', multerSigleUpload.single('image'), (req, res) => {
+
+app.delete('/checkoutdelete/:delete1', multerSigleUpload.single('image'), (req, res) => {
+  var db = "delete from orderdetail_has_product where orderDetail_order_id=" + req.params.delete1
+  sql.query(db, function (err, result) {
+    console.log(db);
+    console.log(result);
+    var db2 = "delete from orderdetail where order_id=" + req.params.delete1
+    sql.query(db2, function (err, result2) {
+      console.log(db2);
+      console.log(result2);
+    });
+  });
+});
+
+app.put('/checkoutedit/:editid', multerSigleUpload.single('image'), (req, res, next) => {
+  var db = "UPDATE orderdetail SET  order_quantity = '"+ req.body.order_quantity +"' WHERE order_id = " + req.params.editid
+  sql.query(db, function (err, result) {
+    console.log(db);
+    console.log(result);
+    var plus = req.body.order_quantity * req.body.order_price
+    var db2 = "UPDATE orderDetail_has_product SET  total_quantity_product_id = '"+ req.body.order_quantity + "' AND total_price_product_id ='"+ plus +"' WHERE orderDetail_order_id =" + req.params.editid
+    sql.query(db2, function (err, result2) {
+      console.log(db2);
+      console.log(result2);
+    });
+  });
+  res.redirect('/');
+});
+
+app.post('/checkout', multerSigleUpload.single('image'), (req, res, next) => {
   if (!req.cookies['jwt']) {
     return res.status(401).send("must login to see cart")
   } else {
@@ -297,23 +318,28 @@ app.delete('/checkoutdelete', multerSigleUpload.single('image'), (req, res) => {
     if (!decoded) {
       return res.status(401).send("unauthebtucated")
     }
-    var db = "delete from orderdetail_has_product where orderDetail_order_id=" + req.body.orderDetail_order_id
+    var db = "select order_id from orderdetail where order_id=(select max(order_id) from orderdetail);"
     sql.query(db, function (err, result) {
       console.log(db);
       console.log(result);
-      var db2 = "delete from orderdetail where order_id=" + req.body.orderDetail_order_id
+      var db2 = "INSERT INTO `orderdetail`( `order_price`, `order_quantity`,`user_user_id`) VALUES ('" + req.body.order_price + "', '" + req.body.order_quantity + "','" + decoded.id + "')";
       sql.query(db2, function (err, result2) {
         console.log(db2);
         console.log(result2);
       });
-    
+      var plus = result[0].order_id + 1;
+      var a = req.body.order_price * req.body.order_quantity
+      var db1 = "INSERT INTO `orderdetail_has_product`(`orderDetail_order_id`, `product_product_id`, `total_price_product_id`,`total_quantity_product_id`) VALUES ('" + plus + "','" + req.body.product_id + "', '" + a + "','" + req.body.order_quantity + "')";
+      sql.connect((err) => {
+        sql.query(db1, function (err, result1) {
+          console.log(db1);
+          console.log(result1);
+        });
       });
-    }
-  
+    });
+  }
   res.redirect('/');
 });
-
-
 
 app.get('/getcheckout', multerSigleUpload.single('image'), (req, res, next) => {
   var db = "SELECT * FROM product p, orderdetail od, orderdetail_has_product odh WHERE od.order_id = odh.orderDetail_order_id AND p.product_id = odh.product_product_id";
@@ -330,14 +356,8 @@ app.get('/getcheckout', multerSigleUpload.single('image'), (req, res, next) => {
 });
 
 app.get('/getcheckoutbyid', multerSigleUpload.single('image'), (req, res, next) => {
-  if (!req.cookies['jwt']) {
-    return res.status(401).send("must login to see cart")
-  } else {
     const theCookie = req.cookies['jwt'];
     const decoded = jwt.verify(theCookie, 'secrect');
-    if (!decoded) {
-      return res.status(401).send("unauthebtucated")
-    }
     var db = "SELECT * FROM product p, orderdetail od, orderdetail_has_product odh WHERE od.order_id = odh.orderDetail_order_id AND p.product_id = odh.product_product_id and od.user_user_id = " + decoded.id;
     sql.connect((err) => {
       sql.query(db, function (err, result1) {
@@ -346,21 +366,10 @@ app.get('/getcheckoutbyid', multerSigleUpload.single('image'), (req, res, next) 
         console.log("kay");
       });
     });
-  }
 });
 
 
-app.put('/checkoutedit/:checkoutid', multerSigleUpload.single('image'), (req, res, next) => {
-  // const theCookie = req.cookies['jwt'];
-  // const decoded = jwt.verify(theCookie, 'secrect');
-  var db = "UPDATE orderdetail SET order_price = '" + req.body.order_price + "' ,  order_quantity = '" + req.body.order_quantity + "' WHERE user_user_id = " + req.params.checkoutid;
-  sql.query(db, function (err, result) {
-    console.log('inserted data');
-    console.log(db);
-    console.log(result);
-  });
-  res.redirect('/');
-});
+
 
 app.post('/orderhasproduct', multerSigleUpload.single('image'), (req, res, next) => {
   // const theCookie = req.cookies['jwt'];
